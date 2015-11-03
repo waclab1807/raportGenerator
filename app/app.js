1e8da44d59910b81ed104ab172e18a5c2685a216
waclaw.labuda@onebi.eu <waclaw.labuda@onebi.eu>
2015-11-03 13:26:56 +0100

    First layout vision before bootstrap

--- app/app.js
+++ app/app.js
@@ -1,12 +1,25 @@
-'use strict';
+// create the module and name it scotchApp
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+// configure our routes
+scotchApp.config(function($routeProvider) {
+  $routeProvider
+
+    // route for the home page
+      .when('/', {
+        templateUrl : 'pages/home.html',
+        controller  : 'mainController'
+      })
+
+    // route for the contact page
+      .when('/contact', {
+        templateUrl : 'pages/raport.html',
+        controller  : 'contactController'
+      });
+});
+
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope) {
+  // create a message to display in our view
+  $scope.message = 'Everyone come and see how good I look!';
+});
---
+++ app/js/methods.js
@@ -0,0 +1,14 @@
+/**
+ * Created by waclab1807 on 02.11.15.
+ */
+//var page = require('webpage').create();
+////var url = 'http://www.google.com';
+//var tmpFunc = function(url){
+//    page.open(url, function(status) {
+//        console.log("Status: " + status);
+//        if(status === "success") {
+//            page.render('example.png');
+//        }
+//        phantom.exit();
+//    });
+//};
